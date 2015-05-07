243ff24bcaf2496fa73b254d9e0049d5911276b4
TychoKerstens <Tychoion@hotmail.com>
2015-05-07 15:51:37 +0200

    Made some more changes

--- adv3.py
+++ adv3.py
@@ -6,12 +6,21 @@ class Room1(object):
 	
 	def enter(self):
 		print "Hi."
-	
+		Player.visited_rooms.append(self)
+		#Haven't foud a way to have this work yet:
+		#Player.visited_room()
+		
+		
 class Room2(object):
+	
+	directions = {
+		'west': 'room1'
+	}
 	
 	def enter(self):
 		print "Hello."
-		
+		Player.visited_rooms.append(self)
+		#Player.visited_room()
 		
 class Room(object):
 	
@@ -20,37 +29,89 @@ class Room(object):
 		'room2': Room2()
 	}
 	
-	def __init__(self, room_name):
-		self.room_name = room_name
+	def __init__(self, current_room, last_room):
+		self.current_room = current_room
+		self.last_room = last_room
 	
 	def start_end(self):
-		current_room = Room1()
-		last_room = self.room_name.next_room('room2')
+		while self.current_room != self.last_room:
 			
-		while current_room != last_room:
-			current_room.enter()
-			print current_room.directions
-			choice = raw_input("> ")
-			current_room = current_room.directions[choice]
-			current_room = self.rooms[current_room]
-			
-			
-class Player(object):
+			if self.current_room in Player.visited_rooms:
+				print Prints.room_prints[0]
+				print self.current_room.directions
+				choice = raw_input("> ")
+				if choice in self.current_room.directions:
+					self.current_room = self.current_room.directions[choice]
+					self.current_room = self.rooms[self.current_room]
+				else:
+					print Prints.room_prints[1]
+			else:
+				self.current_room.enter()
 
-    def __init__(self, HP, armor, damage):
+#For all the raw inputs, haven't made this working yet:
+#class UserInterface(object):
+	
+	#def input(self):
+		#raw_input("> ")
+
+class Calcs(object):
+
+	def player_hit(self):
+		hit = Player.damage - Monster.armor
+		return hit
+		
+	def monster_hit(self):
+		hit = Monster.damage - Player.armor
+		return hit
+	
+	def Monster_HP(self):
+		Monster.hp -= player_hit
+		
+	def Player_HP(self):	
+		Player.HP -= monster_hit
+		
+		
+class Prints(object):
+	
+	general_prints = ["Okidoki.",
+					  "You have been defeated"]
+	
+	room_prints = ["You have explored this room, lets move on.",
+				   "You can't go here. Try again"]
+	
+	equip_prints = ["What item do you want to inspect?",
+					"Do you want to equip this item?",
+					"That item is not in your backpack."]
+	
+	combat_prints = ["You hit the monster for %d damage.",
+	                 "Monster has %d HP left",
+					 "The monster is defeated!",
+					 "Monster strikes back dealing %d damage.",
+					 "You have %d HP left."]
+					
+	def player_stat(self):
+		if Player.HP >= 10:
+			print "You survived. You have %d HP left." % self.HP
+		elif Player.HP > 0:
+			print "You have %d HP left, you survived but you are pretty wounded!" % self.HP
+		elif Player.HP <= 0:
+			print "You have died. Game over."
+	
+
+class Player(object):
+	
+	backpack = {}
+	visited_rooms = []
+	
+	def __init__(self, HP, armor, damage):
 		self.HP = HP
 		self.armor = armor
 		self.damage = damage
+	
+	#def visited_room(self):
+		#visited_rooms.append(Room.current_room)
 		
-    def player_stat(self):
-		if self.HP >= 10:
-			print "You survived. You have %d HP left." % self.HP
-		elif self.HP > 0:
-			print "You have %d HP left, you survived but you are pretty wounded!" % self.HP
-		elif self.HP <= 0:
-			print "You have died. Game over."
-		
-    def player_defeated(self):
+	def player_defeated(self):
 		if self.HP <= 0:
 			return True
 		else:
@@ -79,82 +140,58 @@ class Equipment(object):
 		self.descript = descript
 		self.armor = armor
 		self.damage = damage
-
+		
+		Player.backpack[self.key] = self.descript
+		
 	def see_backpack(self):
-		print Backpack
-		print "What item do you want to inspect?"
+		print Prints.equip_prints[0]
 		see = raw_input("> ")
 		
-		if see in Backpack:
-			print Backpack[see]
-			print "Do you want to equip this item?"
+		if see in Player.backpack:
+			print Player.backpack[see]
+			print Prints.equip_prints[1]
 			equip = raw_input("> ")
-			if equip == "Yes":
+			if equip == "yes":
 				Player.damage = self.damage
 				Player.armor = self.armor
 			else:
-				print "Okidoki."
+				print Prints.general_prints[0]
 				
 		else:
-			print "That item is not in your backpack."
+			print Prints.equip_prints[2]
 			self.see_backpack
 
 		
 def fighting(Monster, Player):
 	while Monster.monster_defeated() == False and Player.player_defeated() == False:
 		
-		player_hit = Player.damage - Monster.armor
-		monster_hit = Monster.damage - Player.armor
-		print "You hit the monster for %d damage." % player_hit
-		Monster.HP -= player_hit
-		print "Monster has %d HP left" % Monster.HP
+		#I broke fighting, when called Calcs.player_hit() gives a TypeError:
+		#"Unbound method player_hit() must be called with Calcs instance as first argument (got nothing instead)"
+		#Same goes for Calcs.Monster_HP() and the rest.
+		#I will try and find a way to make the formats go into the Calcs function
+		print  Prints.combat_prints[0] % Calcs.player_hit()
+		Calcs.Monster_HP()
+		print  Prints.combat_prints[1] % Monster.HP
 		raw_input("> ")
 		
 		if Monster.monster_defeated() == True:
-			print "The monster is defeated!"
+			print Player.combat_prints[2]
 			break
 			
-		print "Monster strikes back dealing %d damage." % monster_hit
-		Player.HP -= monster_hit
-		print "You have %d HP left." % Player.HP
+		print  Prints.combat_prints[3] % Calcs.monster_hit()
+		Calcs.Player_HP()
+		print  Prints.combat_prints[4] % Player.HP
 		raw_input("> ")
 		
 		if Player.player_defeated() == True:
-			print "You have been defeated"
+			print Prints.general_prints[1]
 		#exit(0) for 
 		
 
-class UserInterface(object):
-	pass 
 	
-	
-class Maps(object):
-	
-	rooms = {
-		'room1': Room1(),
-	    'room2': Room2()
-	}
-	
-	def __init__(self, first_room):
-		self.first_room = first_room
-		
-	def next_room(self, room_name):
-		room = Maps.rooms.get(room_name)
-		return room
-	
-	def first_room(self):
-		return self.next_room(self.first_room)
-		
-Backpack = {}
-#Backpack[Equipment.key] = Equipment.descript				
-			
-#fire_axe = Equipment('fire_axe', 
-					#'This fine work of magical crafting from dwarven origins smells and looks like molten magma',
-                    #0, 6)
-#fire_axe.see_backpack()
-
-begin = Maps('room1')
-start = Room(begin)
+adventurer = Player(20, 1, 3)
+orc = Monster(8, 1, 2)	
+start = Room(Room1(), Room2())
 start.start_end()
 
 #while True:
